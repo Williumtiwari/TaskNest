a74b94aabc6f5cf0fb5c1c553283590c6b6b5798
Willium Tiwari <williumtiwari123@gmail.com>
2023-12-15 21:10:44 +0530

    Initial Setup

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,6 @@
+/* trunk-ignore-all(prettier) */
+export default function Home() {
+  return (
+    <p className='text-red-500 bg-black h-[100%]'>Hello Notion Clone</p>
+  )
+}
--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return ( 
-    <p className='text-red-500 fonnt'>Hello Notion Clone</p>
-  )
-}
