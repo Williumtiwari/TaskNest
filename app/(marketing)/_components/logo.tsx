6886ab1fa79add026ba04c4129ed857e683f1e52
Willium Tiwari <williumtiwari123@gmail.com>
2023-12-16 21:52:48 +0530

    Adding Schema, Document page and Created Routes

--- app/(marketing)/_components/logo.tsx
+++ app/(marketing)/_components/logo.tsx
@@ -13,15 +13,15 @@ export const Logo = () => {
     <div className="hidden md:flex items-center gap-x-2">
       <Image
         src="/logo.svg"
-        height="40"
-        width="40"
+        height="30"
+        width="30"
         alt="Logo"
         className="dark:hidden"
       />
       <Image
         src="/logo-dark.svg"
-        height="40"
-        width="40"
+        height="30"
+        width="30"
         alt="Logo"
         className="hidden dark:block"
       />
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,19 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/providers/theme-provider";
-import { ConvexClientProvider } from "@/components/providers/convex-provider";
+import { Toaster } from "sonner";
+import { Inter } from 'next/font/google'
+import type { Metadata } from 'next'
 
-const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { ConvexClientProvider } from '@/components/providers/convex-provider'
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { EdgeStoreProvider } from "@/lib/edgestore";
+
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "TaskNest",
-  description: "The connected workspace where better, faster work happens.",
+  title: 'TaskNest',
+  description: 'The connected workspace where better, faster work happens.',
   icons: {
     icon: [
       {
@@ -28,23 +33,27 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ConvexClientProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-            storageKey="TaskNest-theme-2"
-          >
+          <EdgeStoreProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              enableSystem
+              disableTransitionOnChange
+              storageKey="TaskNest-theme-2"
+            >
+              <Toaster position="bottom-center" />
+              <ModalProvider />
               {children}
-          </ThemeProvider>
+            </ThemeProvider>
+          </EdgeStoreProvider>
         </ConvexClientProvider>
       </body>
     </html>
-  );
+  )
 }
